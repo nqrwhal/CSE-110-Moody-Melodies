e306d3e456e881dea94ad6b1714e4fc5ad88bcbb
Yves-M22 <sebastianYSM12@gmail.com>
2024-11-11 16:51:07 -0800

    hpfilesadded

---
+++ src/components/homepage/hpnew.tsx
@@ -0,0 +1,11 @@
+import './hpnew.css';
+
+const Hpnew = () => {
+    return(
+        <body>
+        <h1 className="mt-3">Moody Melodies</h1>
+        </body>
+    );
+};
+
+export default Hpnew;
---
+++ src/views/HomePageView.tsx
@@ -0,0 +1,17 @@
+import Hpnew from "../components/homepage/hpnew";
+
+/**
+ * @description view of the HomePage
+ * @author Yves Mojica
+ */
+
+
+export const HomePageView = () => {
+  return (
+    <div className="container">
+        <div className="col-sm">
+          <Hpnew />
+        </div>
+    </div>
+  );
+};
