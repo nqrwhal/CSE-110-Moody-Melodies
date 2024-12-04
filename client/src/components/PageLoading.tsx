7b7abc9e61f7352dbdc925ccf8c5c96748164a08
Mark Yan <swiftgapple@hotmail.com>
2024-12-04 14:54:54 -0800

    add a loader when gemini api is loading

---
+++ client/src/components/PageLoading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageLoading = () => {
+    return (
+        <div className="loading-container">
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default PageLoading;
